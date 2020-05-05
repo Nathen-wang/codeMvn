db1334c2d18c3ea1d4e51b8bc99cdd33ec8afdb2
Nathen <ww18256962582@163.com>
2020-05-05 12:52:51 +0800

    添加controller

---
+++ src/main/java/com/webServer/project/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.webServer.project.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
